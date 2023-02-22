61201bbe484577221ba872990279646b79434cb5
Rodrigues Monteiro da Silva <lucas.rodrigues@grupoantolin.com>
2023-02-22 07:21:34 -0300

    mensagem

---
+++ src/main/java/br/com/lucas/lab04/springlab04/Springlab04Application.java
@@ -0,0 +1,13 @@
+package br.com.lucas.lab04.springlab04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlab04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlab04Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucas/lab04/springlab04/Springlab04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.lab04.springlab04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlab04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
